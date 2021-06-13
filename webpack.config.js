d36a5a91be55d0943851faabb0dd18ba91c0f1da
Steven Shipsey <sashipsey@gmail.com>
2021-06-13 01:29:49 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'src/index.html', to: 'index.html' }]
+    })
+  ]
+};
