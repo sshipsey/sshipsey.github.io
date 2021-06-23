818b4e51718debdd601cd4eed5d6c469e823f6ce
Steve Shipsey <sashipsey@gmail.com>
2021-06-23 01:16:02 -0400

    finished version 1 of site

--- webpack.config.js
+++ webpack.config.js
@@ -8,29 +8,38 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    compress: true
+    compress: true,
   },
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: 'src/index.html', to: 'index.html' }]
-    })
-  ]
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/index.html'),
+          to: path.resolve(__dirname, 'dist/index.html'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/img'),
+          to: path.resolve(__dirname, 'dist/img'),
+        },
+      ],
+    }),
+  ],
 };
